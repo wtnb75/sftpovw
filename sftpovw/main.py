5c5e6da2f1098dc28761ac1847f5258b650c80de
Watanabe Takashi <wtnb75@gmail.com>
2025-12-08 21:25:05 +0900

    pathlib

--- sftpovw/main.py
+++ sftpovw/main.py
@@ -4,6 +4,7 @@ import paramiko
 import os
 import json
 from logging import getLogger
+from pathlib import Path
 from .fs import FS
 from .version import VERSION
 
@@ -16,6 +17,13 @@ _log = getLogger(__name__)
 def cli(ctx):
     if ctx.invoked_subcommand is None:
         print(ctx.get_help())
+
+
+class PathParamType(click.ParamType):
+    name = "path"
+
+    def convert(self, value, param, ctx):
+        return Path(value)
 
 
 def verbose_option(func):
@@ -38,19 +46,20 @@ def verbose_option(func):
 
 def ssh_option(func):
     @click.option("--host", required=True)
-    @click.option("--identity-file", type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True))
-    @click.option("--ssh-config", type=click.Path(), default="~/.ssh/config")
+    @click.option("--identity-file", type=PathParamType())
+    @click.option("--ssh-config", type=PathParamType(), default="~/.ssh/config")
     @functools.wraps(func)
-    def _(host, identity_file, ssh_config, **kwargs):
+    def _(host, identity_file: Path | None, ssh_config: Path, **kwargs):
         conf = paramiko.SSHConfig().from_path(os.path.expanduser(ssh_config))
         cfg = conf.lookup(host)
         with paramiko.SSHClient() as ssh:
             ssh.set_missing_host_key_policy(paramiko.WarningPolicy())
             ssh.load_system_host_keys()
+            idf = identity_file and str(identity_file) or cfg.get("identityfile")
             ssh.connect(
                 hostname=cfg["hostname"],
                 username=cfg.get("user"),
-                key_filename=identity_file or cfg.get("identityfile"),
+                key_filename=idf,
                 port=int(cfg.get("port", 22)),
             )
             return func(client=ssh, **kwargs)
@@ -58,52 +67,77 @@ def ssh_option(func):
     return _
 
 
-def copy_option(func):
-    @click.argument("remote-file")
-    @click.argument("local-file")
-    @click.option("--level", type=int, default=3)
-    @functools.wraps(func)
-    def _(**kwargs):
-        return func(**kwargs)
-
-    return _
-
-
 @cli.command()
 @verbose_option
 @ssh_option
-@copy_option
-def get(client: paramiko.SSHClient, remote_file: str, local_file: str, level):
+@click.option("--level", type=int, default=3)
+@click.argument("remote-files", type=PathParamType(), nargs=-1)
+@click.argument("local", type=PathParamType())
+def get(client: paramiko.SSHClient, remote_files: tuple[Path, ...], local: Path, level):
+    """get files from remote"""
     fs = FS(client=client)
-    fs.get(remotepath=remote_file, localpath=local_file, level=level)
+
+    for rf in remote_files:
+        if local.is_dir():
+            ofn = local / rf.name
+            fs.get(remotepath=rf, localpath=ofn, level=level)
+        else:
+            fs.get(remotepath=rf, localpath=local, level=level)
 
 
 @cli.command()
 @verbose_option
 @ssh_option
-@copy_option
-def put(client: paramiko.SSHClient, remote_file: str, local_file: str, level):
+@click.option("--level", type=int, default=3)
+@click.argument("local-files", type=PathParamType(), nargs=-1)
+@click.argument("remote", type=PathParamType())
+def put(client: paramiko.SSHClient, local_files: tuple[Path, ...], remote: Path, level):
+    """put files to remote"""
     fs = FS(client=client)
-    with open(local_file) as fp:
-        fs.put(fp=fp, remotepath=remote_file, level=level)
+    is_dir = fs.is_dir(remote)
+    for lf in local_files:
+        with open(lf, "rb") as fp:
+            if is_dir:
+                remote_path = remote / lf.name
+                fs.put(fp=fp, remotepath=remote_path, level=level)
+            else:
+                fs.put(fp=fp, remotepath=remote, level=level)
 
 
 @cli.command()
 @verbose_option
 @ssh_option
-@click.argument("filenames", nargs=-1)
-def checksum(client: paramiko.SSHClient, filenames: tuple[str]):
+@click.argument("filenames", type=PathParamType(), nargs=-1)
+def checksum(client: paramiko.SSHClient, filenames: tuple[Path]):
+    """checksum remote files"""
     fs = FS(client=client)
     click.echo(json.dumps(fs.hash(filenames)))
 
 
 @cli.command()
 @verbose_option
+@click.argument("filenames", type=PathParamType(), nargs=-1)
+def checksum_local(filenames: tuple[Path]):
+    """checksum local files"""
+    click.echo(json.dumps(FS.hash_local(filenames)))
+
+
+@cli.command()
+@verbose_option
 @ssh_option
-@click.argument("filename")
-def listtmp_remote(client: paramiko.SSHClient, filename: str):
+@click.argument("filename", type=PathParamType())
+def listtmp(client: paramiko.SSHClient, filename: Path):
+    """list remote temporary garbage"""
     fs = FS(client=client)
     click.echo(json.dumps(fs.listtmp(filename)))
+
+
+@cli.command()
+@verbose_option
+@click.argument("filename", type=PathParamType())
+def listtmp_local(filename: Path):
+    """list local temporary garbage"""
+    click.echo(json.dumps(FS.listtmp_local(filename)))
 
 
 if __name__ == "__main__":
