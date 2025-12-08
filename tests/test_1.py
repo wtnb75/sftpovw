720a210ad90aeb66e0b915e4d919d36474cf0546
Watanabe Takashi <wtnb75@gmail.com>
2025-12-08 22:09:07 +0900

    x

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,6 +1,13 @@
 import unittest
+import tempfile
+from pathlib import Path
+from sftpovw.fs import FS
 
 
 class Test1(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(1, 1)
+        tf = tempfile.NamedTemporaryFile("r+b")
+        tf.write(b"hello world")
+        tf.flush()
+        res = FS.hash_local([Path(tf.name)])
+        self.assertEqual(["2aae6c35c94fcfb415dbe95f408b9ce91ee846ed"], list(res.values()))
