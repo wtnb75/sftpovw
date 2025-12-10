299a71945e957ae036b2a63c3e8d9e232338fd14
Watanabe Takashi <wtnb75@gmail.com>
2025-12-11 00:36:37 +0900

    update

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -12,9 +12,7 @@ class TestFS(unittest.TestCase):
         tf.write(b"hello world")
         tf.flush()
         res = FS.hash_local([Path(tf.name)])
-        self.assertEqual(
-            ["2aae6c35c94fcfb415dbe95f408b9ce91ee846ed"], list(res.values())
-        )
+        self.assertEqual(["2aae6c35c94fcfb415dbe95f408b9ce91ee846ed"], list(res.values()))
 
     def test_put_safeN(self):
         with SFTPContainer() as sftp:
@@ -24,10 +22,26 @@ class TestFS(unittest.TestCase):
                 username="basic",
                 password="password",
             )
-            for level in (0, 1, 2, 3):
+            for level in (0, 1, 2, 3, 4):
                 fp = io.BytesIO(b"hello world\n")
+                filelen = fp.getbuffer().nbytes
                 opath = Path("upload/hello.txt")
-                fs.put(fp, opath, level=level)
+                putlen = fs.put(fp, opath, level=level)
+                self.assertEqual(filelen, putlen)
                 with tempfile.NamedTemporaryFile("r+") as tmpf:
-                    fs.get(opath, Path(tmpf.name), level=level)
+                    getlen = fs.get(opath, Path(tmpf.name), level=level)
                     self.assertEqual("hello world\n", Path(tmpf.name).read_text())
+                    self.assertEqual(filelen, getlen)
+
+    def test_isdir(self):
+        with SFTPContainer() as sftp:
+            fs = FS(
+                hostname=sftp.get_container_host_ip(),
+                port=sftp.get_exposed_sftp_port(),
+                username="keypair",
+                key_filename=sftp.users[1].private_key_file,
+            )
+            self.assertTrue(fs.is_dir(Path("upload")))
+            self.assertFalse(fs.is_dir(Path("notfound.txt")))
+            fs.put(io.BytesIO(b"hello\n"), Path("upload/test.txt"))
+            self.assertFalse(fs.is_dir(Path("upload/test.txt")))
